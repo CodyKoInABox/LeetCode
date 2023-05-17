ee942f6dabb5d3e621500c1a2884aa3c13eb9c03
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-05-17 14:37:44 -0300

    Time: 70 ms (37.90%) | Memory: 44.4 MB (62.59%) - LeetSync

---
+++ fizz-buzz/fizz-buzz.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+let fizzBuzz = function(n) {
+    let result = []
+
+    for(let i = 1; i<=n; i++){
+        let string = ""
+
+        if(i%3==0){
+            string += 'Fizz'
+        }
+        if(i%5==0){
+            string += 'Buzz'
+        }
+        else if(i%3!=0 && i%5!=0){
+            string += i
+        }
+
+        result.push(string)
+    }
+
+    return result
+};
