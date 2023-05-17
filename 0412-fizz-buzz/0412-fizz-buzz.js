83005e92e1322edbac0dea4bc4e5eac4e2f47137
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-05-17 14:28:19 -0300

    Time: 64 ms (71.68%), Space: 44.5 MB (44.54%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -8,13 +8,13 @@ let fizzBuzz = function(n) {
     for(let i = 1; i<=n; i++){
         let string = ""
 
-        if(i%3===0){
+        if(i%3==0){
             string += 'Fizz'
         }
-        if(i%5===0){
+        if(i%5==0){
             string += 'Buzz'
         }
-        else if(i%3!==0 && i%5!==0){
+        else if(i%3!=0 && i%5!=0){
             string += i
         }
 
