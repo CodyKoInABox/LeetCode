ff7b9490b85a3b6aa8fbd56edd74047028a1a27e
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-05-20 13:08:18 -0300

    Time: 55 ms (76.70%) | Memory: 41.7 MB (73.52%) - LeetSync

---
+++ number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let counter = 0;
+
+    while(num > 0){
+        counter++
+        
+        if(num%2==0){
+            num = num / 2
+        }else{
+            num = num - 1
+        }
+    }
+
+    return counter
+};
