43bacfa02006cedcf52664a7a5d14e1ad622d06c
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-05-20 20:12:32 -0300

    Time: 2344 ms (41.46%) | Memory: 0B (100.00%) - LeetSync

--- replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 
-SELECT unique_id, name from Employees LEFT JOIN EmployeeUNI 
+SELECT unique_id, name from Employees LEFT JOIN EmployeeUNI
 ON Employees.id = EmployeeUNI.id;
