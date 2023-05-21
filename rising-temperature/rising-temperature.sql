8f198bbebe8ed5c554990b455db00ad2cddeb760
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-05-21 01:05:05 -0300

    Time: 1118 ms (19.63%) | Memory: 0B (100.00%) - LeetSync

--- rising-temperature/rising-temperature.sql
+++ rising-temperature/rising-temperature.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 
 SELECT day1.id FROM Weather day1, Weather day2
-WHERE day1.temperature > day2.temperature AND DATEDIFF(day1.recordDate, day2.recordDate) = 1;
+WHERE day1.temperature > day2.temperature AND DATEDIFF(day1.recordDate, day2.recordDate) = 1; 
