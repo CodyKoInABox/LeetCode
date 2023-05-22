c919f2abb554d441e1fae4d58abfbce94d502ed9
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-05-22 13:47:32 -0300

    Time: 2214 ms (12.14%) | Memory: 0B (100.00%) - LeetSync

--- employee-bonus/employee-bonus.sql
+++ employee-bonus/employee-bonus.sql
@@ -2,4 +2,4 @@
 
 SELECT name, bonus FROM Employee LEFT JOIN Bonus
 ON Employee.empId = Bonus.empId
-WHERE Bonus.bonus < 1000 OR Bonus.bonus IS NULL;
+WHERE Bonus.bonus IS NULL OR Bonus.bonus < 1000;
