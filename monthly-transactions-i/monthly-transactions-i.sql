87f952915144e2bc659b2e7b7be8d52f41c93f62
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-05-23 14:54:37 -0300

    Time: 833 ms (97.19%) | Memory: 0B (100.00%) - LeetSync

--- monthly-transactions-i/monthly-transactions-i.sql
+++ monthly-transactions-i/monthly-transactions-i.sql
@@ -7,5 +7,5 @@ COUNT(id) as trans_count,
 SUM(CASE WHEN state = 'approved' then 1 else 0 end) as approved_count,
 SUM(amount) as trans_total_amount,
 SUM(CASE WHEN state = 'approved' then amount else 0 end) as approved_total_amount
-FROM Transactions t
+FROM Transactions t 
 GROUP BY month, t.country;
