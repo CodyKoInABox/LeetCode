8ae44595dca0084b2ba7047f489883afa81a9074
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-05-23 14:44:05 -0300

    Time: 1588 ms (10.56%) | Memory: 0B (100.00%) - LeetSync

---
+++ queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+
+SELECT q.query_name, ROUND(AVG(q.rating/q.position),2) as quality, ROUND(AVG((rating < 3)*100),2) as poor_query_percentage
+from Queries q 
+GROUP BY q.query_name;
