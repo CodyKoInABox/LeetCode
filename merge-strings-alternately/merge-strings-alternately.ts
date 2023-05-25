8593dd1358427522681dd05bfd8a548cf7d34312
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-05-24 23:18:08 -0300

    Time: 66 ms (46.60%) | Memory: 44.4 MB (68.22%) - LeetSync

---
+++ merge-strings-alternately/merge-strings-alternately.ts
@@ -0,0 +1,11 @@
+function mergeAlternately(word1: string, word2: string): string {
+    let largestLength : number = Math.max(word1.length, word2.length)
+    let output : string = ''
+
+    for(let i : number = 0; i<largestLength; i++){
+        if(word1[i]) output += word1[i]
+	    if(word2[i]) output += word2[i]
+    }
+
+    return output
+};
