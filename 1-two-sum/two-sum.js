34bc185a5974d607c8adcf996b002ed1a548913e
Cody Ko In A Box <codykoinabox@gmail.com>
2023-08-23 02:02:59 -0300

    Time: 128 ms (22.91%) | Memory: 42.3 MB (65.32%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+
+    let ans = [];
+    
+    for(let i = 0; i < nums.length; i++){
+        for(let j = 0; j < nums.length; j++){
+            if(nums[i] + nums[j] == target && i != j){
+                ans[0] = i
+                ans[1] = j
+            }
+        }
+    }
+
+    return ans
+};
