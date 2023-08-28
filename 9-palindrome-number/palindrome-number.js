f7a6fc10a395bc0caf7574cd269da15982837762
Cody Ko In A Box <codykoinabox@gmail.com>
2023-08-28 14:49:05 -0300

    Time: 123 ms (95.82%) | Memory: 51.5 MB (20.88%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+
+    if(x < 0){
+        return false
+    }
+
+    let string = x + '';
+
+    for(let i = 0; i < string.length; i++){
+        if(string[i] != string[string.length-i-1]){
+            return false;
+        }
+    }
+    
+    return true
+};
